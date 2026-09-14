8a539a50c824737f889123ee09a51147c6cd6aa9
Lennon Cehajic <lennon.cehajic@stu.ocsb.ca>
2026-09-14 13:39:18 +0000

    Added hello_world_style.py

---
+++ hello_world_style.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# Created By: Lennon Cehajic
+# Date: September 14th, 2026
+# This program displays a greeting with proper formatting.
+def main():
+    print("Hello, Ms Raffin")
+    print("Programming is very fun")
+
+
+if __name__ == "__main__":
+    main()
